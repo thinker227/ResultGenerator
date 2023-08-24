78b9f8cc2e1a02f48b7fdff560a266f74afb780b
thinker227 <thinker0227@gmail.com>
2023-08-25 01:26:12 +0200

    Add documentation to ReturnsResultAttribute

--- src/ResultGenerator/ReturnsResultAttribute.cs
+++ src/ResultGenerator/ReturnsResultAttribute.cs
@@ -1,9 +1,35 @@
 ﻿namespace ResultGenerator;
 
+/// <summary>
+/// Specifies a method which returns a generated result type.
+/// <br/>
+/// Methods with this attribute should have an attribute list with the <c>result</c> target,
+/// the attribute names of which will specify the variants in the generated result type.
+/// <br/>
+/// Result variants may have parameters, specified as attribute arguments
+/// with a single type argument which specifies the parameter type.
+/// <code>
+/// [ReturnsResult]
+/// [result: Ok(Value&lt;Person&gt;), NotFound]
+/// public GetPersonResult GetPerson(string name)
+/// {
+///     // ...
+/// }
+/// </code>
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
 public sealed class ReturnsResultAttribute : Attribute
 {
-    public ReturnsResultAttribute() {}
-    
-    public ReturnsResultAttribute(string typeName) {}
+    #pragma warning disable IDE0060
+
+    /// <summary>
+    /// Initializes a new <see cref="ReturnsResultAttribute"/> instance.
+    /// </summary>
+    /// <param name="typeName">
+    /// The name of the generated result type.
+    /// <br/>
+    /// If not specified, the generated type name will be
+    /// the name of the target method + <c>Result</c>.
+    /// </param>
+    public ReturnsResultAttribute(string? typeName = null) {}
 }
