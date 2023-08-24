493fd917dd051c63f82f656aa1c5a82d2245b229
thinker227 <thinker0227@gmail.com>
2023-08-24 18:43:27 +0200

    Initial commit

---
+++ src/ResultGenerator.Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using ResultGenerator;
+
+Console.WriteLine("Hello, World!");
+
+S.Hello();
+
+#pragma warning disable CS0658 // Disable invalid attribute target warnings.
+internal static class C
+{
+    [ReturnsResult]
+    [result: TooSmall, TooLarge, Ok]
+    public static void GetNumber(int x) =>
+        throw new NotImplementedException();
+
+    [ReturnsResult]
+    [result: NotFound, Ok<Person>(Value)]
+    public static void GetPerson(string name) =>
+        throw new NotImplementedException();
+
+    public record Person(string Name, int Age);
+}
---
+++ src/ResultGenerator/ReturnsResultAttribute.cs
@@ -0,0 +1,4 @@
+﻿namespace ResultGenerator;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class ReturnsResultAttribute : Attribute {}
