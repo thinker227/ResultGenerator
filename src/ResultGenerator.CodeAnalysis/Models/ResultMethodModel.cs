e2676c505535402189dafd2ba48ecec69973e39d
thinker227 <thinker0227@gmail.com>
2023-08-24 21:02:11 +0200

    Update generator

---
+++ src/ResultGenerator.CodeAnalysis/Models/ResultMethodModel.cs
@@ -0,0 +1,19 @@
+using ResultGenerator.Helpers;
+
+namespace ResultGenerator.Models;
+
+internal readonly record struct ResultMethod(
+    string Name,
+    EquatableArray<ResultValue> Values);
+
+internal readonly record struct ResultValue(
+    string Name,
+    EquatableArray<ValueParameter> Parameters);
+
+internal readonly record struct ValueParameter(
+    string Name,
+    ParameterType Type);
+
+internal readonly record struct ParameterType(
+    string FullyQualifiedMetadataName,
+    bool IsNullable);
