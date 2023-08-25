28d1006607c543ee7bd598ed598885878a5845b4
thinker227 <thinker0227@gmail.com>
2023-08-25 23:12:27 +0200

    Update Program.cs

--- src/ResultGenerator.Sample/Program.cs
+++ src/ResultGenerator.Sample/Program.cs
@@ -2,18 +2,42 @@
 
 Console.WriteLine("Hello, World!");
 
-#pragma warning disable CS0658 // Disable invalid attribute target warnings.
 public sealed class PersonService
 {
+    private readonly Dictionary<string, Person> people = new()
+    {
+        ["Max"] = new("Max", 22),
+        ["Susie"] = new("Susie", 29),
+    };
+
     [ReturnsResult]
     [result: Created, DuplicateName]
-    public void CreatePerson(string name, int age) =>
-        throw new NotImplementedException();
+    // TODO: Update void to CreatePersonResult once that bug is fixed.
+    public void CreatePerson(string name, int age)
+    {
+        var success = people.TryAdd(name, new(name, age));
+
+        // return success
+        //     ? CreatePersonResult.Created()
+        //     : CreatePersonResult.DuplicateName();
+    }
 
     [ReturnsResult("GetPersonResult")]
     [result: Ok(Value<Person>), NotFound]
-    public void GetPersonByName(string name) =>
-        throw new NotImplementedException();
+    public GetPersonResult GetPersonByName(string name)
+    {
+        var person = name switch
+        {
+            "Max" => new Person("Max", 22),
+            "Rick" => new Person("Rick", 36),
+            "Susie" => new Person("Susie", 29),
+            _ => null,
+        };
+
+        return person is not null
+            ? GetPersonResult.Ok(person)
+            : GetPersonResult.NotFound();
+    }
 
     public record Person(string Name, int Age);
 }
