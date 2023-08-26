4ecd9151c6af11b91fc7bd83a0da8a8bc5572505
thinker227 <thinker0227@gmail.com>
2023-08-26 14:43:55 +0200

    Update analyzer

--- src/ResultGenerator.CodeAnalysis/Analysis/Analyzer.cs
+++ src/ResultGenerator.CodeAnalysis/Analysis/Analyzer.cs
@@ -8,8 +8,9 @@ namespace ResultGenerator.Analysis;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class Analyzer : DiagnosticAnalyzer
 {
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
-        ImmutableArray.Create(Diagnostics.SpecifyResultList);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+        Diagnostics.SpecifyResultDeclaration,
+        Diagnostics.TooManyResultDeclarations);
 
     public override void Initialize(AnalysisContext ctx)
     {
@@ -25,16 +26,20 @@ public sealed class Analyzer : DiagnosticAnalyzer
             {
                 var symbol = (IMethodSymbol)symbolStartCtx.Symbol;
 
+                if (symbol.DeclaringSyntaxReferences[0]!.GetSyntax() is not MethodDeclarationSyntax symbolSyntax) return;
+
                 var attribute = symbol.GetAttributes().FirstOrDefault(attr =>
-                    attr.AttributeClass!.Equals(
+                    attr.AttributeClass?.Equals(
                         typeProvider.ReturnsResultAttribute,
-                        SymbolEqualityComparer.Default));
+                        SymbolEqualityComparer.Default)
+                        ?? false);
 
                 if (attribute is null) return;
 
-                var attributeSyntax = attribute.ApplicationSyntaxReference!;
+                var attributeSyntax = (AttributeSyntax)attribute.ApplicationSyntaxReference!
+                    .GetSyntax();
                 
-                var resultDeclaration = symbol.DeclaringSyntaxReferences
+                var resultDeclarations = symbol.DeclaringSyntaxReferences
                     .Select(r => r.GetSyntax())
                     .OfType<MethodDeclarationSyntax>()
                     .SelectMany(node => node.AttributeLists)
@@ -43,13 +48,27 @@ public sealed class Analyzer : DiagnosticAnalyzer
 
                 symbolStartCtx.RegisterSymbolEndAction(symbolEndCtx =>
                 {
-                    if (resultDeclaration.Length == 0)
+                    if (resultDeclarations.Length == 0)
                     {
-                        symbolEndCtx.ReportDiagnostic(Diagnostic.Create(
-                            Diagnostics.SpecifyResultList,
-                            Location.Create(attributeSyntax.SyntaxTree, attributeSyntax.Span)));
+                        symbolEndCtx.ReportDiagnostic(
+                            Diagnostic.Create(
+                                Diagnostics.SpecifyResultDeclaration,
+                                symbolSyntax.Identifier.GetLocation()));
+                    }
+
+                    if (resultDeclarations.Length > 1)
+                    {
+                        foreach (var decl in resultDeclarations.Skip(1))
+                        {
+                            symbolEndCtx.ReportDiagnostic(
+                                Diagnostic.Create(
+                                    Diagnostics.TooManyResultDeclarations,
+                                    decl.GetLocation()));
+                        }
                     }
                 });
+
+
             }, SymbolKind.Method);
         });
     }
--- src/ResultGenerator.CodeAnalysis/Analysis/Diagnostics.cs
+++ src/ResultGenerator.CodeAnalysis/Analysis/Diagnostics.cs
@@ -5,15 +5,19 @@ namespace ResultGenerator.Analysis;
 
 public static class Diagnostics
 {
-    public static DiagnosticDescriptor SpecifyResultList { get; } = new(
+    public static DiagnosticDescriptor SpecifyResultDeclaration { get; } = new(
         "RESGEN0001",
-        "Specify result attribute",
-        "Specify an attribute [result: ...]",
+        "Specify result declaration",
+        "Specify a result declaration using [result: ...]",
         "ResultCorrectness",
         DiagnosticSeverity.Warning,
-        isEnabledByDefault: true,
-        description:
-            "{0} should specify an attribute [result: ...] " + 
-            "because it has the [ReturnsResult] attribute applied, " +
-            "otherwise the attribute will have no effect");
+        isEnabledByDefault: true);
+
+    public static DiagnosticDescriptor TooManyResultDeclarations { get; } = new(
+        "RESGEN0002",
+        "Too many result declarations",
+        "Only one result declaration may be specified per method",
+        "ResultCorrectness",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
 }
