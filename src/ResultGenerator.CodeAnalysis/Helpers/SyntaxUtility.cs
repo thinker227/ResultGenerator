e46d265bc0cb2cf60858e1e05fa213728b165191
thinker227 <thinker0227@gmail.com>
2023-08-26 17:52:48 +0200

    Fix bug in SyntaxUtility

--- src/ResultGenerator.CodeAnalysis/Helpers/SyntaxUtility.cs
+++ src/ResultGenerator.CodeAnalysis/Helpers/SyntaxUtility.cs
@@ -5,6 +5,7 @@ namespace ResultGenerator.Helpers;
 public static class SyntaxUtility
 {
     public static bool IsValidIdentifier(string identifier) =>
+        identifier.Length >= 1 &&
         SyntaxFacts.IsIdentifierStartCharacter(identifier[0]) &&
         identifier.Skip(1).All(SyntaxFacts.IsIdentifierPartCharacter);
 }
