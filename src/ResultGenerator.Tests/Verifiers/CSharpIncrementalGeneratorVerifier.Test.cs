20fe77c32432e4ca2a317bf13ceba79ad5d47ad2
thinker227 <thinker0227@gmail.com>
2023-08-29 19:28:55 +0200

    Hotfix to disable markup for generator tests

--- src/ResultGenerator.Tests/Verifiers/CSharpIncrementalGeneratorVerifier.Test.cs
+++ src/ResultGenerator.Tests/Verifiers/CSharpIncrementalGeneratorVerifier.Test.cs
@@ -1,7 +1,9 @@
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Model;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace ResultGenerator.Tests.Verifiers;
@@ -32,6 +34,18 @@ internal static partial class CSharpIncrementalGeneratorVerifier<TIncrementalGen
         protected override ParseOptions CreateParseOptions()
         {
             return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(LanguageVersion);
+        }
+
+        protected override async Task RunImplAsync(CancellationToken cancellationToken)
+        {
+            var analyzers = GetDiagnosticAnalyzers().ToArray();
+            var defaultDiagnostic = GetDefaultDiagnostic(analyzers);
+            var supportedDiagnostics = analyzers.SelectMany(analyzer => analyzer.SupportedDiagnostics).ToImmutableArray();
+            var fixableDiagnostics = ImmutableArray<string>.Empty;
+            var testState = TestState.WithInheritedValuesApplied(null, fixableDiagnostics);
+
+            var diagnostics = await VerifySourceGeneratorAsync(testState, Verify, cancellationToken).ConfigureAwait(false);
+            await VerifyDiagnosticsAsync(new EvaluatedProjectState(testState, ReferenceAssemblies).WithAdditionalDiagnostics(diagnostics), testState.AdditionalProjects.Values.Select(additionalProject => new EvaluatedProjectState(additionalProject, ReferenceAssemblies)).ToImmutableArray(), testState.ExpectedDiagnostics.ToArray(), Verify.PushContext("Diagnostics of test state"), cancellationToken).ConfigureAwait(false);
         }
     }
 }
