32072557d7185a7fe2815883149120d21f2f60ec
thinker227 <thinker0227@gmail.com>
2023-08-30 22:09:25 +0200

    Update WellKnownTypeProvider

--- src/ResultGenerator/WellKnownTypeProvider.cs
+++ src/ResultGenerator/WellKnownTypeProvider.cs
@@ -3,14 +3,19 @@ using Microsoft.CodeAnalysis;
 namespace ResultGenerator;
 
 internal record WellKnownTypeProvider(
-    INamedTypeSymbol ReturnsResultAttribute)
+    INamedTypeSymbol ReturnsResultAttribute,
+    INamedTypeSymbol ResultTypeAttribute)
 {
     public static WellKnownTypeProvider? Create(Compilation compilation)
     {
         var returnsResultAttribute = compilation.GetTypeByMetadataName("ResultGenerator.ReturnsResultAttribute");
+        var resultTypeAttribute = compilation.GetTypeByMetadataName("ResultGenerator.Internal.ResultTypeAttribute");
 
         if (returnsResultAttribute is null) return null;
+        if (resultTypeAttribute is null) return null;
 
-        return new(returnsResultAttribute);
+        return new(
+            returnsResultAttribute,
+            resultTypeAttribute);
     }
 }
