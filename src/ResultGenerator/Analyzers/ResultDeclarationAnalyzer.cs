7e89404886088e2953407dba6142627a9b3a07c7
thinker227 <thinker0227@gmail.com>
2023-08-31 17:19:07 +0200

    Cleanup

--- src/ResultGenerator/Analyzers/ResultDeclarationAnalyzer.cs
+++ src/ResultGenerator/Analyzers/ResultDeclarationAnalyzer.cs
@@ -21,7 +21,7 @@ public sealed class ResultDeclarationAnalyzer : DiagnosticAnalyzer
 
     public override void Initialize(AnalysisContext ctx)
     {
-        // ctx.EnableConcurrentExecution();
+        ctx.EnableConcurrentExecution();
         ctx.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 
         ctx.RegisterTypeProviderAction(typeProviderCtx =>
