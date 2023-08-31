02327e78ea3bb463175bf76007d6652db3b692fc
thinker227 <thinker0227@gmail.com>
2023-08-31 10:50:41 +0200

    Move types

---
+++ src/ResultGenerator/ResultTypeDeclarationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ResultGenerator;
+
+/// <summary>
+/// Represents a declaring attribute for a result type. 
+/// </summary>
+/// <param name="Data">The data for the attribute.</param>
+/// <param name="Syntax">The syntax for the attribute.</param>
+/// <param name="ConstructArguments">The constructor configuration of the attribute.</param>
+internal readonly record struct ResultTypeDeclarationAttribute(
+    AttributeData Data,
+    AttributeSyntax Syntax,
+    AttributeCtorArgs ConstructArguments);
