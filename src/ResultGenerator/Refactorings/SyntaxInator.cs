d19b4c1a8ed204240469ee38178bb623e1a6d052
thinker227 <thinker0227@gmail.com>
2023-09-03 16:35:06 +0200

    Add ToResultRefactoring

---
+++ src/ResultGenerator/Refactorings/SyntaxInator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace ResultGenerator.Refactorings;
+
+internal static class SyntaxInator
+{
+    public static AttributeListSyntax ReturnsResultAttribute() =>
+        AttributeList(
+            SingletonSeparatedList(
+                Attribute(
+                    IdentifierName("ReturnsResult"))));
+
+    public static AttributeListSyntax DefaultResultDeclarationWithParameter(TypeSyntax okParameterType) =>
+        AttributeList(
+                SeparatedList<AttributeSyntax>(
+                    new SyntaxNodeOrToken[]{
+                        Attribute(
+                            IdentifierName("Ok"))
+                        .WithArgumentList(
+                            AttributeArgumentList(
+                                SingletonSeparatedList(
+                                    AttributeArgument(
+                                        GenericName(
+                                            Identifier("Value"))
+                                        .WithTypeArgumentList(
+                                            TypeArgumentList(
+                                                SingletonSeparatedList(
+                                                    okParameterType))))))),
+                        Token(SyntaxKind.CommaToken),
+                        Attribute(
+                            IdentifierName("Error"))}))
+            .WithTarget(
+                AttributeTargetSpecifier(
+                    Identifier("result")));
+
+    public static ExpressionSyntax WrapExpressionWithOk(string resultTypeName, ExpressionSyntax? expression)
+    {
+        var invocation = InvocationExpression(
+            MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                IdentifierName(resultTypeName),
+                IdentifierName("Ok")));
+
+        var argumentList = expression is not null
+            ? SingletonSeparatedList(
+                Argument(
+                    expression))
+            : default;
+
+        return invocation.WithArgumentList(
+            ArgumentList(
+                argumentList));
+    }
+}
---
+++ src/ResultGenerator/Refactorings/ToResultRefactoring.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace ResultGenerator.Refactorings;
+
+[ExportCodeRefactoringProvider(LanguageNames.CSharp)]
+public sealed class ToResultRefactoring : CodeRefactoringProvider
+{
+    public override async Task ComputeRefactoringsAsync(CodeRefactoringContext ctx)
+    {
+        var document = ctx.Document;
+
+        var root = await document.GetSyntaxRootAsync();
+
+        if (root?.FindNode(ctx.Span) is not MethodDeclarationSyntax methodDeclaration) return;
+
+        var semanticModel = await document.GetSemanticModelAsync(ctx.CancellationToken);
+        var compilation = semanticModel!.Compilation;
+        var typeProvider = WellKnownTypeProvider.Create(compilation);
+        if (typeProvider is null) return;
+
+        if (semanticModel.GetDeclaredSymbol(methodDeclaration, ctx.CancellationToken) is not IMethodSymbol method) return;
+
+        // Refactoring should not be available for methods which already declare a result type.
+        if (ResultTypeDeclaringMethod.Create(
+                method,
+                typeProvider,
+                null,
+                checkPartialDeclarations: true)
+            is not null) return;
+        
+        ctx.RegisterRefactoring(CodeAction.Create(
+            "Change return type to result",
+            cts => Execute(
+                cts,
+                document,
+                semanticModel,
+                method,
+                methodDeclaration)));
+    }
+
+    private static async Task<Document> Execute(
+        CancellationToken cts,
+        Document document,
+        SemanticModel semanticModel,
+        IMethodSymbol methodSymbol,
+        MethodDeclarationSyntax methodDeclaration)
+    {
+        var name = Result.GetResultTypeName(methodSymbol);
+        var returnTypeSymbol = methodSymbol.ReturnType;
+        var returnType = methodDeclaration.ReturnType;
+
+        // Add attributes.
+        var returnsResultAttribute = SyntaxInator.ReturnsResultAttribute()
+            .WithAdditionalAnnotations(Formatter.Annotation);
+
+        var resultDeclaration = SyntaxInator.DefaultResultDeclarationWithParameter(returnType)
+            .WithAdditionalAnnotations(Formatter.Annotation);
+
+        // Change return type.
+        var newReturnType = SyntaxFactory.IdentifierName(name)
+            .WithAdditionalAnnotations(Formatter.Annotation);
+
+        var newExpressionBody = null as ArrowExpressionClauseSyntax;
+        var newBody = null as BlockSyntax;
+
+        // Update expression body.
+        if (methodDeclaration.ExpressionBody is ArrowExpressionClauseSyntax {
+            Expression: var expressiobBodyExpr
+        } expressionBody)
+        {
+            var wrappedExpr = SyntaxInator.WrapExpressionWithOk(name, expressiobBodyExpr);
+            newExpressionBody = expressionBody.WithExpression(wrappedExpr);
+        }
+        // Update block body.
+        else if (methodDeclaration.Body is BlockSyntax body &&
+            semanticModel.GetOperation(body, cts) is IBlockOperation bodyOperation)
+        {
+            var returnStatements = bodyOperation.Descendants()
+                .OfType<IReturnOperation>()
+                .Select(ret => (ReturnStatementSyntax)ret.Syntax);
+            
+            newBody = body.ReplaceNodes(
+                returnStatements,
+                (node, _) => UpdateReturnStatement(node, name)
+                    .WithAdditionalAnnotations(Formatter.Annotation));
+        }
+
+        var newMethodDeclaration = methodDeclaration
+            .AddAttributeLists(
+                returnsResultAttribute,
+                resultDeclaration)
+            .WithReturnType(newReturnType)
+            .WithExpressionBody(newExpressionBody)
+            .WithBody(newBody);
+
+        var oldRoot = await document.GetSyntaxRootAsync(cts);
+        var newRoot = oldRoot!.ReplaceNode(methodDeclaration, newMethodDeclaration);
+
+        return document.WithSyntaxRoot(newRoot);
+    }
+
+    private static ReturnStatementSyntax UpdateReturnStatement(
+        ReturnStatementSyntax returnStatement,
+        string resultTypeName)
+    {
+        var expression = SyntaxInator.WrapExpressionWithOk(
+            resultTypeName,
+            returnStatement.Expression);
+
+        return returnStatement.WithExpression(expression);
+    }
+}
